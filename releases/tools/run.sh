8c87b5e059c64a6367928408ec3b89250e51f442
enomem <enomem@substation>
2025-02-20 15:47:45 +0300

    Modifications in run.sh tools and spec environment settings

--- releases/tools/run.sh
+++ releases/tools/run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-_spec_file="$1"
-_spec_file_template="${_spec_file}".template
+_spec_file_template="$1"
+
+_spec_file="${_spec_file_template%.template}"
 _spec_file_env="${_spec_file}".env
+_spec_file="${_spec_file##*/}"
 
 _debug="$2"
 
@@ -10,15 +12,18 @@ _debug="$2"
 
 _usage()
 {
-	echo "usage: $(basename "$0") <file.spec>"
+	echo "usage: $(basename "$0") <spec.template> [debug]"
 }
 
 [[ $# -lt 1 ]] && { _usage; exit 1; }
 
 _ensure_log()
 {
-	local log_file=$(realpath -m "$1")
-	local log_dir=$(dirname "$log_file")
+	local log_file
+	local log_dir
+	log_file=$(realpath -m "$1")
+	log_dir=$(dirname "$log_file")
+
 	[[ -d "$log_dir" ]] || ${_DEBUGP} mkdir -p "$log_dir"
 	[[ -f "$log_file" ]] || ${_DEBUGP} touch "$log_file"
 }
@@ -27,15 +32,18 @@ _ensure_source_subpath()
 {
 	local spec_file="$1"
 	local repo_root="$2"
-	local source_subpath=$(cat "${spec_file}" | grep '^source_subpath: ' | sed -e 's/source_subpath: //')
-	local source_subpath_latest=$(ls "${repo_root:-.}"/var/tmp/catalyst/builds/${source_subpath/-latest/*} | grep -v '\-latest' | sort -r | head -1)
+	local source_subpath
+	local source_subpath_latest
+	source_subpath=$(cat "${spec_file}" | grep '^source_subpath: ' | sed -e 's/source_subpath: //')
+	source_subpath_latest=$(ls "${repo_root:-.}"/var/tmp/catalyst/builds/"${source_subpath/-latest/*}" | grep -v '\-latest' | sort -r | head -1)
 	[[ -n "${source_subpath_latest}" ]] && ln -fs -T "${source_subpath_latest}" "${repo_root:-.}"/var/tmp/catalyst/builds/"${source_subpath}"
 }
 
 _ensure_snapshot()
 {
 	local repo_root="$1"
-	local snapshot_latest=$(ls "${repo_root:-.}"/var/tmp/catalyst/snapshots/*.xz.sqfs | grep -v 'latest.xz.sqfs' | sort -r | head -1)
+	local snapshot_latest
+	snapshot_latest=$(ls "${repo_root:-.}"/var/tmp/catalyst/snapshots/*.xz.sqfs | grep -v 'latest.xz.sqfs' | sort -r | head -1)
 	[[ -n "${snapshot_latest}" ]] && ln -fs -T "${snapshot_latest}" "${repo_root:-.}"/var/tmp/catalyst/snapshots/gentoo-latest.xz.sqfs
 }
 
@@ -46,44 +54,56 @@ _ensure_portdir()
 	cp -r "${repo_root}"/etc/portage/default/* "${portage_confdir}"/
 }
 
-_get_repo_root()
+_ensure_confdir()
 {
-	local spec_file_env=$1
-	env -i bash -c '{ source "'${spec_file_env}'"; echo "${REPO_ROOT}"; }'
+	local catalyst_conf_dir="$1"
+	mkdir -p "${catalyst_conf_dir}"
 }
 
-_get_catalyst_conf()
+_get_spec_file_variable()
 {
-	local spec_file_env=$1
-	env -i bash -c '{ source "'${spec_file_env}'"; echo "${CATALYST_CONF}"; }'
+    local spec_file_env=$1
+    local spec_file_variable=$2
+    env -i bash -c '{ source "'"${spec_file_env}"'"; echo "${'"${spec_file_variable}"'}"; }'
 }
 
-_get_catalyst_log()
-{
-	local spec_file_env=$1
-	env -i bash -c '{ source "'${spec_file_env}'"; echo "${CATALYST_LOG}"; }'
-}
+_repo_root=$(_get_spec_file_variable "${_spec_file_env}" REPO_ROOT)
+_catalyst_conf_template=$(_get_spec_file_variable "${_spec_file_env}" CATALYST_CONF_TEMPLATE)
+_catalyst_conf_dir=$(_get_spec_file_variable "${_spec_file_env}" CATALYST_CONF_DIR)
+_catalyst_log=$(_get_spec_file_variable "${_spec_file_env}" CATALYST_LOG)
+_portage_confdir=$(_get_spec_file_variable "${_spec_file_env}" PORTAGE_CONFDIR)
+_makeopts=$(_get_spec_file_variable "${_spec_file_env}" MAKEOPTS)
+_distcc_hosts=$(_get_spec_file_variable "${_spec_file_env}" DISTCC_HOSTS)
 
-_get_portage_confdir()
-{
-	local spec_file_env=$1
-	env -i bash -c '{ source "'${spec_file_env}'"; echo "${PORTAGE_CONFDIR}"; }'
-}
+_catalyst_conf_template_file="${_catalyst_conf_template##*/}"
+_catalyst_conf="${_catalyst_conf_dir}"/"${_catalyst_conf_template_file%.template}"
 
-_repo_root=$(_get_repo_root "${_spec_file_env}")
-_catalyst_conf=$(_get_catalyst_conf "${_spec_file_env}")
-_catalyst_log=$(_get_catalyst_log "${_spec_file_env}")
-_portage_confdir=$(_get_portage_confdir "${_spec_file_env}")
-
-if [[ -f "${_spec_file_template}" ]] && [[ -f "${_spec_file_env}" ]]; then
-	cat "${_spec_file_template}" |
-		env -i bash -c '{ source "'${_spec_file_env}'"; envsubst; }' \
-	> "${_spec_file}"
+if [[ -f "${_catalyst_conf_template}" ]] && [[ -f "${_spec_file_env}" ]] && \
+   [[ -n "${_catalyst_conf_dir}" ]]; then
+    	_ensure_confdir "${_catalyst_conf_dir}"
+	cat "${_catalyst_conf_template}" |
+		env -i bash -c '{ source "'"${_spec_file_env}"'"; envsubst; }' \
+	> "${_catalyst_conf}"
 fi
 
-if [[ -f "${_spec_file}" ]] && \
+if [[ -n "${_makeopts}" ]] && [[ -n "${_distcc_hosts}" ]] && \
+   [[ -d "${_catalyst_conf_dir}" ]]; then
+	echo "export MAKEOPTS='${_makeopts}'" \
+	     > "${_catalyst_conf_dir}"/catalystrc
+	echo "export DISTCC_HOSTS='${_distcc_hosts}'" \
+	     >> "${_catalyst_conf_dir}"/catalystrc
+fi
+
+if [[ -f "${_spec_file_template}" ]] && [[ -f "${_spec_file_env}" ]] && \
+   [[ -d "${_catalyst_conf_dir}" ]]; then
+	cat "${_spec_file_template}" |
+		env -i bash -c '{ source "'"${_spec_file_env}"'"; envsubst; }' \
+	> "${_catalyst_conf_dir}"/"${_spec_file}"
+fi
+
+if [[ -f "${_catalyst_conf_dir}"/"${_spec_file}" ]] && \
    [[ -n "${_repo_root}" ]]; then
-	_ensure_source_subpath "${_spec_file}" "${_repo_root}"
+	_ensure_source_subpath "${_catalyst_conf_dir}"/"${_spec_file}" "${_repo_root}"
 	_ensure_snapshot "${_repo_root}"
 fi
 
@@ -92,9 +112,9 @@ if [[ -n "${_portage_confdir}" ]] && \
 	_ensure_portdir "${_repo_root}" "${_portage_confdir}"
 fi
 
-if [[ -f "${_spec_file}" ]] && \
-   [[ -n "${_catalyst_conf}" ]] && \
+if [[ -f "${_catalyst_conf_dir}"/"${_spec_file}" ]] && \
+   [[ -f "${_catalyst_conf}" ]] && \
    [[ -n "${_catalyst_log}" ]]; then
    	_ensure_log "${_catalyst_log}"
-	${_DEBUGP} catalyst -a -d -c "${_catalyst_conf}" --log-file "${_catalyst_log}" -f "${_spec_file}" &> "${_catalyst_log%.log}.err"
+	${_DEBUGP} catalyst -a -d -c "${_catalyst_conf}" --log-file "${_catalyst_log}" -f "${_catalyst_conf_dir}"/"${_spec_file}" &> "${_catalyst_log%.log}.err"
 fi
