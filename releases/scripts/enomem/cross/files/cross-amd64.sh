f385ae83d8dd1170d810a14e37a50ad58d447f27
enomem <enomem@substation>
2026-07-13 23:44:35 +0300

    Update stage4 catalyst specs [1/∞]

--- releases/scripts/enomem/cross/files/cross-amd64.sh
+++ releases/scripts/enomem/cross/files/cross-amd64.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # cross x86_64-unknown-linux-gnu
-crossdev --b 2.45.1 --g 15.2.1_p20251108-r1 --k 6.17 --l 2.42-r2 -t x86_64-unknown-linux-gnu || exit 1
+crossdev --b 2.46.1 --g 16.1.0 --k 6.19 --l 2.43-r2 -t x86_64-unknown-linux-gnu || exit 1
 
 # cross x86_64-pc-linux-musl
-crossdev --b 2.45.1 --g 15.2.1_p20251108-r1 --k 6.17 --l 1.2.5-r6 -t x86_64-pc-linux-musl || exit 1
+crossdev --b 2.46.1 --g 16.1.0 --k 6.19 --l 1.2.6-r1 -t x86_64-pc-linux-musl || exit 1
 
 # cross armv6j-unknown-linux-gnueabihf
-crossdev --b 2.45.1 --g 15.2.1_p20251108-r1 --k 6.17 --l 2.42-r2 -t armv6j-unknown-linux-gnueabihf || exit 1
+crossdev --b 2.46.1 --g 16.1.0 --k 6.19 --l 2.43-r2 -t armv6j-unknown-linux-gnueabihf || exit 1
 
 # cross riscv64-unknown-linux-gnu
-crossdev --b 2.45.1 --g 15.2.1_p20251108-r1 --k 6.17 --l 2.42-r2 -t riscv64-unknown-linux-gnu || exit 1
+crossdev --b 2.46.1 --g 16.1.0 --k 6.19 --l 2.43-r2 -t riscv64-unknown-linux-gnu || exit 1
